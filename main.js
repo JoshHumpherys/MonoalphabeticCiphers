024cb6188a192f1e9aed977ee16777317a2e23a8
Josh Humpherys <joshua.humpherys@gmail.com>
2017-01-18 18:10:01 -0500

    Reorder letters according to English frequencies

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let str = 'Elwvim yickgltizkjc rh jvznroc xzhvw lm ezgjvezgryzo gjvlic zmw ylekfgvi hyrvmyv kizygryv yickgltizkjry zotlirgjeh ziv wvhrtmvw zilfmw ylekfgzgrlmzo jziwmvhh zhhfekgrlmh ezprmt hfyj zotlirgjeh jziw gl xivzp rm kizygryv xc zmc zwnvihzic Rg rh gjvlivgryzooc klhhrxov gl xivzp hfyj z hchgve xfg rg rh rmuvzhrxov gl wl hl xc zmc pmldm kizygryzo evzmh Gjvhv hyjvevh ziv gjvivuliv gvievw ylekfgzgrlmzooc hvyfiv gjvlivgryzo zwnzmyvh zmw uzhgvi ylekfgrmt gvyjmloltc ivsfriv gjvhv hlofgrlmh gl xv ylmgrmfzooc zwzkgvw Gjviv vbrhg rmuliezgrlmgjvlivgryzooc hvyfiv hyjvevh gjzg kilnzxoc yzmmlg xv xilpvm vnvm drgj fmorergvw ylekfgrmt kldvizm vbzekov rh gjv lmvgrev kzwxfg gjvhv hyjvevh ziv eliv wruuryfog gl rekovevmg gjzm gjv xvhg gjvlivgryzooc xivzpzxov xfg ylekfgzgrlmzooc hvyfiv evyjzmrheh';
+let englishFrequencies = [0.08167,0.01492,0.02782,0.04253,0.12702,0.0228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02360,0.00150,0.01974,0.00074];
+let englishLetterOrdering = 'etaoinshrdlcumwfgypbvkjxqz';
+let englishLetterOrderingArray = englishLetterOrdering.split('');
 
 function shiftString(str, shift) {
     let array = str.toLowerCase().split('');
     let aCharCode = 'a'.charCodeAt(0);
     let zCharCode = 'z'.charCodeAt(0);
-    for(var i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
         if(/^[a-z]*$/.test(array[i])) {
             array[i] = (array[i].charCodeAt(0) + shift);
             if(array[i] > zCharCode) {
@@ -22,11 +25,11 @@ function reverseString(str) {
 
 function calculateLetterOccurrences(str) {
     let occurences = {};
-    for(var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    for(let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
         occurences[String.fromCharCode(i)] = 0;
     }
     let array = str.toLowerCase().split('');
-    for(var i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
         if(/^[a-z]*$/.test(array[i])) {
             occurences[array[i]] += 1;
         }
@@ -37,7 +40,7 @@ function calculateLetterOccurrences(str) {
 function calculateLetterFrequencies(str) {
     let frequencies = {};
     let occurences = calculateLetterOccurrences(str);
-    for(var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    for(let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
         let char = String.fromCharCode(i);
         frequencies[char] = occurences[char] / str.length;
     }
@@ -46,7 +49,7 @@ function calculateLetterFrequencies(str) {
 
 function calculateMean(array) {
     let sum = 0;
-    for(var i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
         sum += array[i];
     }
     return sum / array.length;
@@ -55,10 +58,38 @@ function calculateMean(array) {
 function calculateStandardDeviation(array) {
     let mean = calculateMean(array);
     let sum = 0;
-    for(var i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
         sum += Math.pow(array[i] - mean, 2);
     }
     return Math.sqrt(sum / array.length);
+}
+
+function calculateOrdering(array) {
+    let ordering = [];
+    for(let i = 0; i < array.length; i++) {
+        let largestValue = -1;
+        let largestIndex = -1;
+        for(let j = 0; j < array.length; j++) {
+            if(array[j] > largestValue) {
+                largestValue = array[j];
+                largestIndex = j;
+            }
+        }
+        ordering[largestIndex] = i;
+        array[largestIndex] = -1;
+    }
+    return ordering;
+}
+
+function reorderLetters(str, ordering) {
+    let array = str.toLowerCase().split('');
+    let aCharCode = 'a'.charCodeAt(0);
+    for(let i = 0; i < array.length; i++) {
+        if(/^[a-z]*$/.test(array[i])) {
+            array[i] = englishLetterOrderingArray[ordering[array[i].charCodeAt(0) - aCharCode]];
+        }
+    }
+    return array.join('');
 }
 
 function main() {
@@ -73,5 +104,9 @@ function main() {
     let frequencyValues = Object.keys(frequencies).map(function(key) {
         return frequencies[key];
     });
-    console.log(calculateStandardDeviation(frequencyValues));
+    // console.log(calculateStandardDeviation(frequencyValues));
+    let ordering = calculateOrdering(frequencyValues);
+    // console.log(ordering);
+    console.log(reorderLetters(str, ordering));
+    // console.log(frequencies);
 }
